6e530d6bcada3a45c60bb6e7cc54178986bd6677
houssam72 <brothersmarket72@gmail.com>
2024-03-30 12:54:51 +0100

    add crud api

--- src/main/java/com/houssam/Main.java
+++ src/main/java/com/houssam/Main.java
@@ -1,21 +1,64 @@
 package com.houssam;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.Period;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customer")
 public class Main {
 
-    public static void main(String[] args){
+
+     @Autowired
+     private CustomerRepository customerRepository;
+     public static void main(String[] args){
         SpringApplication.run(Main.class,args);
     }
+
+    @GetMapping
+    public List<Customer> getCustomer(){
+        return this.customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(String name,String email,Integer age){}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+
+         Customer customer=new Customer();
+         customer.setName(request.name);
+         customer.setEmail(request.email);
+         customer.setAge(request.age);
+
+        this.customerRepository.save(customer);
+
+    }
+
+    @PutMapping("{CustomerId}")
+    public Optional<Customer> replaceEmployee(@RequestBody NewCustomerRequest request, @PathVariable("CustomerId") Integer id) {
+
+       return this.customerRepository.findById(id).map(
+               customer -> {
+                   customer.setAge(request.age);
+                   customer.setName(request.name);
+                   customer.setEmail(request.email);
+                   return this.customerRepository.save(customer);
+               }
+       );
+
+    }
+
+    @DeleteMapping("{CustomerId}")
+    public void deleteCustomer(@PathVariable("CustomerId") Integer id){
+         this.customerRepository.deleteById(id);
+    }
+
 
     @GetMapping("/greet")
     public Greet greet(){
