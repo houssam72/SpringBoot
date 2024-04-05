1c3d66bdbb9dc19f708f87f2bf96d6a5e54494fc
houssam72 <brothersmarket72@gmail.com>
2024-04-05 16:53:27 +0200

    feat: add open feign rest client and logging SLF4J

--- src/main/java/com/houssam/Main.java
+++ src/main/java/com/houssam/Main.java
@@ -3,11 +3,13 @@ package com.houssam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.*;
 
 //import java.util.List;
 
 @SpringBootApplication
+@EnableFeignClients
 //@RestController
 public class Main {
 
--- src/main/java/com/houssam/customer/CustomerService.java
+++ src/main/java/com/houssam/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.houssam.customer;
 
 import com.houssam.Exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.Optional;
 @Service
 public class CustomerService {
 
+    private final static Logger LOGGER=  LoggerFactory.getLogger(CustomerService.class);
+
     @Autowired
 //    @Qualifier("fake")
     public CustomerRepo customerRepo;
@@ -17,13 +21,17 @@ public class CustomerService {
 //    record NewCustomerRequest(String name,String email,Integer age){}
 
     public List<Customer> getCustomer(){
+        LOGGER.info("getCustomers was called");
         return this.customerRepo.findAll();
     }
 
     public Optional<Customer> getCustomerById(Integer id){
 
-        if(!this.customerRepo.findById(id).isPresent())
-            throw new NotFoundException( "Customet with :"+ id+" N=not found");
+        if(!this.customerRepo.findById(id).isPresent()) {
+            NotFoundException notFoundException = new NotFoundException("Customet with :" + id + " N=not found");
+            LOGGER.error("error in getting customer {}",id,notFoundException.toString());
+            throw notFoundException;
+        }
 
             return this.customerRepo.findById(id);
     }
